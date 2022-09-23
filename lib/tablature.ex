9a1c89875c0f300d899b3f82d913ee9fd60b1a1d
Salchiis <d1360_audi@hotmail.com>
2022-09-23 11:23:52 -0500

    Add tablature la romaza

--- lib/tablature.ex
+++ lib/tablature.ex
@@ -1,23 +1,28 @@
 defmodule Tablature do
   def parse(tab) do
-
     tab
-    |> String.split
+    |> String.split()
     |> Enum.map(fn line -> parse_line(line) end)
-    # |> Enum.join(" ")
-    # |> String.trim()
-    |> Enum.filter(fn lista -> lista != [] end)
-    |> Enum.zip
-    |> Enum.map(fn tupla -> Tuple.to_list(tupla) end)
     |> List.flatten()
+    |> Enum.sort_by(fn {_c, p} -> p end)
+    |> Enum.map(fn {c, _p} -> c end)
+    # Concatena todas las cadenas y usa espacio vacio como seaprador
     |> Enum.join(" ")
+
+    # |> String.trim()
+    # |> Enum.filter(fn lista -> lista != [] end)
+    # |> Enum.zip()
+    # |> Enum.map(fn tupla -> Tuple.to_list(tupla) end)
+    # |> List.flatten()
+    # |> Enum.join(" ")
   end
 
   def parse_line(line) do
     note = String.at(line, 0)
-    Regex.scan(~r/\d+/, line)
-    |> List.flatten
-    |> Enum.map(fn str -> note <> str end)
-    # |> Enum.join(" ")
+
+    String.graphemes(line)
+    |> Enum.with_index()
+    |> Enum.filter(fn tupla -> elem(tupla, 0) =~ ~r/\d/ end)
+    |> Enum.map(fn {c, p} -> {note <> c, p} end)
   end
 end
